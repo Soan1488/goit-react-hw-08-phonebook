928d33525720e14ab75585bfac198137647d2b5b
Soan1488 <andrisokolovskiy@gmail.com>
2023-01-14 15:10:39 +0000

    Deploying to gh-pages from @ Soan1488/goit-react-hw-08-phonebook@53f4da3590e641c6b10aa5a3f66c0b1c36b44b37 🚀

---
+++ static/js/544.0195cdc2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[544],{544:function(e,n,o){o.r(n),o.d(n,{default:function(){return c}});var t=o(184);function c(){return(0,t.jsx)("div",{children:(0,t.jsx)("p",{children:"Welcome Home"})})}}}]);
+//# sourceMappingURL=544.0195cdc2.chunk.js.map
